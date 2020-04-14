64641ef2319502e2b59a2f93d83e63f8d0e765d7
Matt LeFort <matthewlefort@gmail.com>
2020-04-14 12:01:11 -0700

    started modal gallery slideshow

--- js/index.js
+++ js/index.js
@@ -3,19 +3,62 @@ window.$ = window.jQuery = jQuery; // workaround for https://github.com/parcel-b
 import popper from 'popper.js';
 
 const numberImages = 13;
-
 const image_list = document.querySelector('#gallery-items');
 let imagesHTML = '';
 let i;
 for (i = 1; i < numberImages; i++) {
-	imagesHTML = imagesHTML.concat(`
+  imagesHTML = imagesHTML.concat(`
   <div class="col-6 col-sm-4 col-md-3 gallery-item p-0 m-0">
-  <a class="example-image-link" href="/img/slides/${i}.png" data-lightbox="example-set" data-title="">
-    <figure class="effect-apollo my-0">
-      <img src="/img/slides/${i}.png" alt="Game Robo" class="img-fluid">
+    <figure class="my-0">
+      <img src="/img/slides/${i}.png" alt="Horseback rides" class="img-fluid hover-shadow" onclick="openModal();currentSlide(${1})">
     </figure>
-  </a>
   </div>`);
 }
-console.log(imagesHTML);
 image_list.innerHTML = imagesHTML;
+
+// Modal functions
+// Open the Modal
+window.openModal = function openModal() {
+  console.log(document.getElementById('myModal'));
+  document.getElementById('myModal').style.display = 'block';
+};
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+}
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName('mySlides');
+//   var dots = document.getElementsByClassName('demo');
+//   var captionText = document.getElementById('caption');
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].className += ' active';
+//   captionText.innerHTML = dots[slideIndex - 1].alt;
+// }
