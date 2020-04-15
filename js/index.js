3f4561ba2eda58d5d16c8b95412c418c580e3cc5
Matt LeFort <matthewlefort@gmail.com>
2020-04-15 11:59:50 -0700

    Finished modal gallery js, next is package layout

--- js/index.js
+++ js/index.js
@@ -24,41 +24,36 @@ window.openModal = function openModal() {
 };
 
 // Close the Modal
-function closeModal() {
+window.closeModal = function closeModal() {
   document.getElementById('myModal').style.display = 'none';
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+window.plusSlides = function plusSlides(n) {
+  showSlides((slideIndex += n));
+};
+
+// Thumbnail image controls
+window.currentSlide = function currentSlide(n) {
+  showSlides((slideIndex = n));
+};
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  console.log(slides);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  // hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
 }
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName('mySlides');
-//   var dots = document.getElementsByClassName('demo');
-//   var captionText = document.getElementById('caption');
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none';
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(' active', '');
-//   }
-//   slides[slideIndex - 1].style.display = 'block';
-//   dots[slideIndex - 1].className += ' active';
-//   captionText.innerHTML = dots[slideIndex - 1].alt;
-// }
